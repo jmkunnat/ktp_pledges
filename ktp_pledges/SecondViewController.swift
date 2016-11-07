1bca21159c901c1660dcf8994f07309fd09a4eae
jmkunnat <jmkunnat@umich.edu>
2016-11-06 22:29:43 -0500

    first add

---
+++ ktp_pledges/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ktp_pledges
+//
+//  Created by Joe Kunnath on 11/6/16.
+//  Copyright © 2016 Joe Kunnath. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
